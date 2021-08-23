7dcda7b610f5ee10acad29ebb684854f17fb8aaa
moolike-material <kobako.material@gmail.com>
2021-08-23 23:52:03 +0900

    aboutページ追加

--- pages/howtow.vue
+++ pages/howtow.vue
@@ -9,7 +9,9 @@
     <h1 class="p-content_subTtl p-content_subTtl--underline">moolikeについて/About</h1>
     <div class="p-sub_content">
       <div class="page__block">
-        <h3 class="page__subttl">moolikeは商用利用OKのシンプルな動画素材サイトです。</h3>
+        <div class="page__sub">
+          <h3 class="page__subttl">moolikeは商用利用OKのシンプルな動画素材サイトです。</h3>
+        </div>
         <p class="page__desc">
           moolikeは運営者である私、pei.nagaeが動画制作を学習する上で生まれた動画素材を配信しているウェブサイトです。<br>
           配信動画はシンプルで使いやすい物をピックアップし提供をしています。<br>
@@ -18,15 +20,92 @@
         </p>
       </div>
       <div class="page__block">
-        <h3 class="page__subttl">運営者 pei.nagaeについて</h3>
-        <p class="page__desc">
-            仕事の傍、動画編集の学習をしている者です。<br>
-            使用ツールは主にAfter efects,premiereを使用しております。
-            大学時代にモーショングラフィックス系の動画作成を経験し、それ以降コソコソと作成続けています。<br>
-            社会人になった現在、webの知識をある程度習得したので当サイトを立ち上げ、運営するに至りました。<br>
-            シンプルなものや可愛いもの、ナチュラルなものが好きなので、そのテイストを中心とした素材制作をしていますので、興味ある方はぜひダウンロードをお願いいたします。
-        </p>
+        <div class="page__sub">
+          <h3 class="page__subttl">運営者 pei.nagaeについて</h3>
+        </div>
+        <div class="prof_wrap">
+          <div>
+            <p class="page__desc">
+              この度は当サイトに訪問いただきありがとうございます^^*<br>
+              仕事の傍、動画編集の学習をしている<span class="p-form__message__link">pei.nagae</span>と申します。<br>
+              シンプルなものや可愛いもの、ナチュラルなものが好きなので、<br>そんな感じのテイストの素材をひっそりと制作しておりますー。<br>
+              興味ある方はぜひダウンロードをお願いいたします。
+            </p>
+            <p class="page__desc">
+              その他個別作成依頼は随時受け付けています。<br>
+              youtube編集、サプライズ動画、結婚式ムービーなど作成をさせていただきますので<br>
+              興味のある方はぜひ<nuxt-link to="/contact/" class="p-form__message__link">お問い合わせフォーム</nuxt-link>よりご相談ください。
+            </p>
+          </div>
+          <div class="prof_image">
+            <img src="/pei.png" alt="pei.nagaeの写真">
+          </div>
+        </div>
       </div>
+    </div>
+    <div class="btn">
+      <NuxtLink to="/" class="btn__link">トップに戻る</NuxtLink>
     </div>
   </div>
 </template>
+
+<style>
+  .page__subttl {
+    display: inline-block;
+    position: relative;
+    z-index: 3;
+  }
+
+  /* 
+  .page__sub {
+    display: inline-block;
+    position: relative;
+  }
+
+  .page__sub::before {
+    position: absolute;
+    content: '';
+    width: 100%;
+    height: 30%;
+    opacity: .9;
+    background: #f7e229;
+    bottom: 0;
+    left: 0;
+    transform:translateX(-2px);
+    z-index: 0;
+  } */
+
+  .prof_wrap {
+    display: flex;
+
+  }
+
+  .p-sub_content {
+    width: 1200px;
+    margin: 0 auto;
+  }
+
+  .prof_image {
+    width: 15%;
+    margin-left: 4rem;
+  }
+
+  @media screen and (max-width:750px) {
+    .prof_wrap {
+      display: block;
+
+    }
+
+    .p-sub_content {
+      width: 340px;
+      margin: 0 auto;
+    }
+
+    .prof_image {
+      width: 50%;
+      margin: 3rem auto;
+    }
+
+  }
+
+</style>
