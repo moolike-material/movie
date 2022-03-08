e45176afe731a93bdb318b3794fb57bd731c4417
mac <mac@macnoiMac.local>
2022-03-08 13:01:48 +0900

    追加

--- pages/contact.vue
+++ pages/contact.vue
@@ -21,8 +21,8 @@
             href="https://www.moolike-stock.com/privacy">プライバシーポリシー</a>を一読の上お進みください</p>
       </div>
       <form name="contact" method="POST" netlify class="form">
-        <input type="hidden" name="form-name" value="contact" />
         <p class="form_item">
+        <input type="hidden" name="form-name" value="contact" />
           <label class="form__label">お名前(ニックネーム可): <span class="form__attention">必須</span><input type="text" name="name" class="form_1coltext" /></label>
         </p>
         <p class="form_item">
