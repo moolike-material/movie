60ea489ba49304fd8090730243329e7ec6764d7e
mac <mac@macnoiMac.local>
2022-03-12 08:23:04 +0900

    zip化

--- layouts/error.vue
+++ layouts/error.vue
@@ -7,9 +7,11 @@
       {{ otherError }}
     </h1>
     <p>お探しのページは見つかりませんでした。</p>
-    <NuxtLink to="/">
-      トップに戻る
+    <div class="p-more_btn u-mT4">
+    <NuxtLink to="/" class="p-more_btn__link">
+      トップに戻る　　
     </NuxtLink>
+    </div>
   </div>
 </template>
 
